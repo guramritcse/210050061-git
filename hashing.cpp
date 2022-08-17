b5c1a43b27c2c91e78fa8872a68f924413af29fa
Guramrit Singh <guramritsingh0403@gmail.com>
2022-08-17 21:38:08 +0530

    Update parameters of hash function

--- hashing.cpp
+++ hashing.cpp
@@ -1,9 +1,8 @@
 #include "hashing.hpp"
 
-int hash_string(string s) {
+int hash_string(string s, int m) {
     // implement hashing function here
     int sum=0;
-    int m=61;
     for(int i=0; i<s.size(); i++)
     {
       sum = (sum + s[i])%m;
